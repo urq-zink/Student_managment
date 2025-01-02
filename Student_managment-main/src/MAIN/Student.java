119dfec01d4fa4e17cdbec8289187af3836b9be0
liemgiaochu <liemgiaochu@gmail.com>
2025-01-02 23:05:46 +0700

    Add files via upload

---
+++ Student_managment-main/src/MAIN/Student.java
@@ -0,0 +1,48 @@
+package qlsv;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String major;
+
+    public Student(int id, String name, int age, String major) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
+
---
+++ Student_managment-main/src/MANAGER/LoginListener.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MANAGER;
+
+import GUI.Menu;
+import GUI.Login;
+
+/**
+ *
+ * @author zink.urq00
+ */
+
+public class LoginListener implements GUI.LoginListener{
+    private Login loginScreen;
+    
+    public void setLoginScreen(Login screen) {
+        this.loginScreen = screen;
+    }
+    
+    @Override
+    public void onLoginSuccess() {
+        if (loginScreen != null) {
+            loginScreen.dispose(); // Đóng Login window 
+            
+            // Mở Home window
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new Menu().setVisible(true);
+                }
+            });
+        }
+    }
+    
+}
